d6193b3234a4de8677a222b5474670648c5a6e98
darthspectrum <muradianator@gmail.com>
2024-05-05 21:38:19 +0000

    chore: implemented a token space

---
+++ seac/lib/token_space.ex
@@ -0,0 +1,46 @@
+defmodule SeaC.TokenSpace do
+  alias SeaC.TokenSpace
+
+  @type t :: %TokenSpace{elements: [any]}
+  defstruct elements: []
+
+  @spec new() :: TokenSpace.t()
+  def new do
+    %TokenSpace{elements: [[]]}
+  end
+
+  @spec squash(TokenSpace.t()) :: TokenSpace.t()
+  def squash(token_space) do
+    case token_space.elements do
+      [singleton] ->
+        %TokenSpace{elements: [singleton]}
+
+      [first | [next | rest]] ->
+        %TokenSpace{elements: [next ++ [first]] ++ rest}
+    end
+  end
+
+  @spec pop(TokenSpace.t()) :: {TokenSpace.t(), any()}
+  def pop(token_space) do
+    case token_space.elements do
+      [first | rest] -> {first, %TokenSpace{elements: rest}}
+      _ -> []
+    end
+  end
+
+  @spec extend(TokenSpace.t()) :: TokenSpace.t()
+  def extend(token_space) do
+    %TokenSpace{elements: [[]] ++ token_space.elements}
+  end
+
+  @spec append(TokenSpace.t(), any()) :: TokenSpace.t()
+  def append(token_space, item) do
+    case token_space.elements do
+      [first | rest] ->
+        %TokenSpace{elements: [first ++ [item]] ++ rest}
+
+      _ ->
+        %TokenSpace{elements: [[item]]}
+    end
+  end
+end
---
+++ seac/test/token_space_test.exs
@@ -0,0 +1,36 @@
+defmodule SeaC.TokenSpaceTests do
+  alias SeaC.TokenSpace
+  use ExUnit.Case
+
+  test "that we are able to create an empty TokenSpace" do
+    assert TokenSpace.new() == %TokenSpace{elements: [[]]}
+  end
+
+  test "that we can squash the top of the TokenSpace" do
+    token_space = %TokenSpace{elements: [[1, 2, 3], [4, 5]]}
+    squashed_token_space = %TokenSpace{elements: [[4, 5, [1, 2, 3]]]}
+
+    assert TokenSpace.squash(token_space) == squashed_token_space
+  end
+
+  test "that we can extend the TokenSpace" do
+    token_space = %TokenSpace{elements: [[1]]}
+    extended_token_space = %TokenSpace{elements: [[], [1]]}
+
+    assert TokenSpace.extend(token_space) == extended_token_space
+  end
+
+  test "that we can append to the top list of the TokenSpace" do
+    token_space = %TokenSpace{elements: [[9, 8], [1, 2, 3]]}
+    appended_token_space = %TokenSpace{elements: [[9, 8, 4], [1, 2, 3]]}
+
+    assert TokenSpace.append(token_space, 4) == appended_token_space
+  end
+
+  test "that we can pop from the TokenSpace" do
+    token_space = %TokenSpace{elements: [[1, 2, 3], [4, 5]]}
+    popped_token_space = %TokenSpace{elements: [[4, 5]]}
+
+    assert TokenSpace.pop(token_space) == {[1, 2, 3], popped_token_space}
+  end
+end
