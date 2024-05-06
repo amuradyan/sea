3de6c3f15121d5c87266adf5be5187f66f3956b7
darthspectrum <muradianator@gmail.com>
2024-05-06 21:43:04 +0000

    chore: testing definitions in lambdas
    * cleanup

--- seac/test/runner_test.exs
+++ seac/test/runner_test.exs
@@ -6,4 +6,10 @@ defmodule SeaC.RunnerTests do
 
     assert SeaC.Runner.run(file) == 3
   end
+
+  test "that we can understand a definition in a function" do
+    file = "test/fixtures/definitions/define-in-a-function.sea"
+
+    assert SeaC.Runner.run(file) == 6
+  end
 end
