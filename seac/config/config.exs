1d3721e0fa81a873cd899ad01f456b8999b8fa2d
darthspectrum <muradianator@gmail.com>
2024-06-08 15:34:03 +0000

    chore: changes to Sea files triggers the tests

--- seac/config/config.exs
+++ seac/config/config.exs
@@ -1,5 +1,10 @@
 import Config
 
+if Mix.env() in [:dev, :test] do
+  config :mix_test_watch,
+    extra_extensions: [".sea"]
+end
+
 config :logger,
   backends: [:console, {LoggerFileBackend, :error_log}]
 
