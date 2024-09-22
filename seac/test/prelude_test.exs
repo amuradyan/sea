d3103a829eaf44ec2c8643824a219125751b2778
darthspectrum <muradianator@gmail.com>
2024-09-22 22:15:53 +0000

    feat: we can map

--- seac/test/prelude_test.exs
+++ seac/test/prelude_test.exs
@@ -2,12 +2,21 @@ defmodule SeaC.PreludeTests do
   use ExUnit.Case
 
   describe "Prelude" do
+    @tag :prelude
+    test "map" do
+      file = "test/fixtures/prelude/map.sea"
+
+      assert SeaC.Runner.run(file) == [[2, 3, 4], []]
+    end
+
+    @tag :prelude
     test "list" do
       file = "test/fixtures/prelude/list.sea"
 
       assert SeaC.Runner.run(file) == [1, 2, :պնդուկ, [1, 2]]
     end
 
+    @tag :prelude
     test "identity" do
       file = "test/fixtures/prelude/identity.sea"
 
