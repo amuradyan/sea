7df7ab6a22c9b4223780408c7fc0d8b3bfac7b97
darthspectrum <muradianator@gmail.com>
2024-09-25 07:50:34 +0000

    chore: test tags

--- seac/test/evaluator_test.exs
+++ seac/test/evaluator_test.exs
@@ -5,6 +5,7 @@ defmodule SeaC.EvaluatorTests do
   alias SeaC.ReservedWords
 
   describe "Evaluator" do
+    @tag :evaluator
     test "that we can extend the environment by defining names for values" do
       env = [[[:four], [4]]]
       x_as_1 = [:define, :x, :"1"]
@@ -16,6 +17,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.define(y_as_lambda, env) == [[[:y], [value]], [[:four], [4]]]
     end
 
+    @tag :evaluator
     test "that we understand the meaning of definitions" do
       env = []
       x_plus_1 = [[:define, :x, :"1"], [:+, :x, :"1"]]
@@ -23,16 +25,19 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.meaning(x_plus_1, env) == 2
     end
 
+    @tag :evaluator
     test "that we regard the numbers as constants" do
       assert Evaluator.meaning(:"7", []) == 7
       assert Evaluator.meaning(:"7.7", []) == 7.7
     end
 
+    @tag :evaluator
     test "that we regard the Booleans as constants" do
       assert Evaluator.meaning(true, []) == true
       assert Evaluator.meaning(false, []) == false
     end
 
+    @tag :evaluator
     test "that we regard some words as constants" do
       actions_for_reserved_words =
         Enum.map(ReservedWords.all(), fn word -> Evaluator.meaning(word, []) end)
@@ -44,6 +49,7 @@ defmodule SeaC.EvaluatorTests do
       assert (actions_for_reserved_words -- expected_outcome) |> Enum.empty?()
     end
 
+    @tag :evaluator
     test "that we regard some expressions as identifiers" do
       identifier = :birthmarks
       env = [[[:scars, identifier], [4, :none]]]
@@ -55,15 +61,18 @@ defmodule SeaC.EvaluatorTests do
       assert unresolved_identifier == "Unable to resolve unknown"
     end
 
+    @tag :evaluator
     test "that we regard some expressions as atoms" do
       assert Evaluator.meaning([:quote, :this], []) == :this
       assert Evaluator.meaning([:quote, [:this, :as, :well]], []) == [:this, :as, :well]
     end
 
+    @tag :evaluator
     test "that we regard some atoms as numbers" do
       assert Evaluator.meaning([:quote, :"2"], []) == 2
     end
 
+    @tag :evaluator
     test "that we regard some expressions as anonymous functions" do
       env = []
       lambda = [:lambda, [:a, :b], [:+, :a, :b]]
@@ -72,6 +81,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.meaning(lambda, env) == value
     end
 
+    @tag :evaluator
     test "that we can interpret Sea functions as atoms" do
       assert Evaluator.atom?(true) == true
       assert Evaluator.atom?(:ping) == true
@@ -80,6 +90,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.atom?([:"non-primitive", :function]) == true
     end
 
+    @tag :evaluator
     test "that we are able to apply primitive functions" do
       assert Evaluator.apply_primitive(:cons, [1, [2]]) == [1, 2]
       assert Evaluator.apply_primitive(:cons, [1, 2]) == [1, 2]
@@ -109,6 +120,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.apply_primitive(:-, [1, 2]) == -1
     end
 
+    @tag :evaluator
     test "that we are able to find the meaning of a primitive" do
       env = [
         [[:list], [[1, [2]]]],
@@ -170,6 +182,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.meaning([:ne?, :mek, :mek], env) == false
     end
 
+    @tag :evaluator
     test "that we can tell a primitive call from a non-primitive one and nest them" do
       closure_table = []
       closure_formals = [:x, :y]
@@ -180,6 +193,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.apply_function([:"non-primitive", closure], [5, 8], []) == [1, 13]
     end
 
+    @tag :evaluator
     test "that we are able to evaluate complex applications" do
       closure_formals = [:x, :y]
       closure_body = [:cons, [:+, :x, :y], :"1"]
@@ -190,6 +204,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.application(application, []) == [11, 1]
     end
 
+    @tag :evaluator
     test "that we are able to calculate the values of expressions" do
       program = [
         [
@@ -215,6 +230,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.value(program) == 10
     end
 
+    @tag :evaluator
     test "that we can evaluate a list" do
       env = [[[:x], [2]]]
 
@@ -222,21 +238,25 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.evaluate_list([], env) == []
     end
 
+    @tag :evaluator
     test "that we gracefully handle unknown types of expressions" do
       assert Evaluator.meaning("des tination", []) == :unknown
     end
 
+    @tag :evaluator
     test "that we are able to check whether an atom is a number" do
       assert Evaluator.number?(:"3") == true
       assert Evaluator.number?(:"3a") == false
     end
 
+    @tag :evaluator
     test "that we are able to distinguish `else` clauses in conditions" do
       assert Evaluator.else_clause?([:else, [:body]]) == true
       assert Evaluator.else_clause?([:ololo, [:body]]) == false
       assert Evaluator.else_clause?([]) == false
     end
 
+    @tag :evaluator
     test "that we can fall back to `else` clause, while evaluating the condition" do
       env = [[[:always, :one], [false, 1]]]
 
@@ -246,6 +266,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.evaluate_clauses([false_clause, else_clause], env) == 1
     end
 
+    @tag :evaluator
     test "that we are able to evaluate condition clauses" do
       env = [[[:always, :sometimes, :two], [false, true, 2]]]
 
@@ -256,6 +277,7 @@ defmodule SeaC.EvaluatorTests do
       assert Evaluator.evaluate_clauses([false_clause, true_clause, else_clause], env) == 2
     end
 
+    @tag :evaluator
     test "that we handle stubs properly" do
       program = [[:define, :stub, [:lambda, [:x], [:"???"]]], [:stub, :"1"]]
 
--- seac/test/token_space_test.exs
+++ seac/test/token_space_test.exs
@@ -2,10 +2,12 @@ defmodule SeaC.TokenSpaceTests do
   alias SeaC.TokenSpace
   use ExUnit.Case
 
+  @tag :token_space
   test "that we are able to create an empty TokenSpace" do
     assert TokenSpace.new() == %TokenSpace{elements: [[]]}
   end
 
+  @tag :token_space
   test "that we can squash the top of the TokenSpace" do
     token_space = %TokenSpace{elements: [[1, 2, 3], [4, 5]]}
     squashed_token_space = %TokenSpace{elements: [[4, 5, [1, 2, 3]]]}
@@ -13,6 +15,7 @@ defmodule SeaC.TokenSpaceTests do
     assert TokenSpace.squash(token_space) == squashed_token_space
   end
 
+  @tag :token_space
   test "that we can extend the TokenSpace" do
     token_space = %TokenSpace{elements: [[1]]}
     extended_token_space = %TokenSpace{elements: [[], [1]]}
@@ -21,6 +24,7 @@ defmodule SeaC.TokenSpaceTests do
     assert TokenSpace.extend(TokenSpace.new()) == TokenSpace.new()
   end
 
+  @tag :token_space
   test "that we can append to the top list of the TokenSpace" do
     token_space = %TokenSpace{elements: [[9, 8], [1, 2, 3]]}
     appended_token_space = %TokenSpace{elements: [[9, 8, 4], [1, 2, 3]]}
@@ -28,6 +32,7 @@ defmodule SeaC.TokenSpaceTests do
     assert TokenSpace.append(token_space, 4) == appended_token_space
   end
 
+  @tag :token_space
   test "that we can pop from the TokenSpace" do
     token_space = %TokenSpace{elements: [[1, 2, 3], [4, 5]]}
     popped_token_space = %TokenSpace{elements: [[4, 5]]}
