9bc7f1ffe6e9bc37e1d322e5f46298d5ac65c88e
darthspectrum <muradianator@gmail.com>
2024-09-26 09:42:32 +0000

    feat: running Sea more conveniently

--- seac/lib/runner.ex
+++ seac/lib/runner.ex
@@ -2,6 +2,7 @@ defmodule SeaC.Runner do
   alias SeaC.Evaluator
   alias SeaC.Tokenizer
 
+<<<<<<< HEAD
   def run(file) do
     {:ok, symbols} = File.read(file)
 
@@ -9,6 +10,16 @@ defmodule SeaC.Runner do
     # this should not be here
     |> hd()
     |> Tokenizer.remove_parens()
+=======
+  def run(symbols) do
+    Tokenizer.process(symbols)
+>>>>>>> b761346 (feat: running Sea more conveniently)
     |> Evaluator.value()
+  end
+
+  def run_file(file) do
+    {:ok, symbols} = File.read(file)
+
+    run(symbols)
   end
 end
--- seac/test/sandbox_test.exs
+++ seac/test/sandbox_test.exs
@@ -11,7 +11,7 @@ defmodule SeaC.SandboxTests do
     test "doing stuff" do
       file = "test/fixtures/sandbox.sea"
 
-      assert SeaC.Runner.run(file) == [
+      assert SeaC.Runner.run_file(file) == [
                [[1, 2], [2, 3], [3, 4]],
                [[4, 1], [3, 2], [2, 3], [1, 4]],
                [[1, 3], [2, 3], [3, 3], [4, 3]],
