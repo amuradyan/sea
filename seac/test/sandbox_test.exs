67930dd0e41bf853dac30f9298294807f6d72f8e
darthspectrum <muradianator@gmail.com>
2024-09-27 18:17:29 +0000

    sandbox: solving for 4 queens on a 4x4 board
    * Board size hardcoded

--- seac/test/sandbox_test.exs
+++ seac/test/sandbox_test.exs
@@ -6,12 +6,16 @@ defmodule SeaC.SandboxTests do
     test "doing stuff" do
       file = "test/fixtures/sandbox.sea"
 
-      assert SeaC.Runner.run_file(file) == [
-               [[1, 2], [2, 3], [3, 4]],
-               [[4, 1], [3, 2], [2, 3], [1, 4]],
-               [[1, 3], [2, 3], [3, 3], [4, 3]],
-               [[2, 1], [2, 2], [2, 3], [2, 4]]
-             ]
+      assert SeaC.Runner.run_file(file) ==
+               [
+                 [],
+                 [],
+                 :none,
+                 [[1, 1]],
+                 :none,
+                 [[1, 1], [2, 3]],
+                 [[1, 2], [2, 4], [3, 1], [4, 3]]
+               ]
     end
   end
 end
